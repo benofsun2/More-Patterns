47845da81084e5fb1709118c4fe0cef2d0d0110a
benofsun2 <benofsun@gmail.com>
2017-05-03 14:40:06 -0700

    Some Pattern Examples
    Dependancy Injection with Unity and Spring

---
+++ DIExampleUnity/DIExampleUnity/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+
+
+namespace DIExampleUnity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IUnityContainer con = new UnityContainer();
+            con.RegisterType<Customer>();
+            con.RegisterType<IDal,SQLServerDal>();
+            con.RegisterType<IDal,OricalServerDal>();
+
+            Customer c = con.Resolve<Customer>();
+            c.CName = "Tets customer";
+            c.Add();
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Customer
+    {
+        public Customer(IDal o)
+        {
+            Odal = o;
+        }
+        private IDal Odal;   
+        public string CName { get; set; }
+
+        public void Add() 
+        {
+            // dal
+            Odal.Add();
+        }
+    }
+
+    public class SQLServerDal : IDal
+    {
+        public void Add()
+        {
+            // dal
+            Console.WriteLine("insertedSQL");
+        }
+    }
+
+    public class OricalServerDal : IDal
+    {
+        public void Add()
+        {
+            // dal
+            Console.WriteLine("insertedOrical");
+        }
+    }
+
+    public interface IDal
+    {
+        void Add();
+    }
+}
